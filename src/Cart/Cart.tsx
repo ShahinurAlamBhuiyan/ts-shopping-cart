3a3844d6038e3801eb7a69bfae627d5731ecedd3
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2021-07-23 02:53:20 +0600

    add site screenshot for readme file

--- src/Cart/Cart.tsx
+++ src/Cart/Cart.tsx
@@ -8,7 +8,7 @@ type Props = {
     removeFromCart: (id: number) => void;
 }
 
-const Cart: React.FC<Props> = ({ cartItems, addToCart, removeFromCart }) => {
+const Cart: React.FC<Props> = ({ cartItems, addToCart, removeFromCart}) => {
 
     const calculateTotal = (items: CartItemType[]) =>  items.reduce((ack: number, item) => ack + item.amount * item.price, 0);
     
@@ -23,7 +23,7 @@ const Cart: React.FC<Props> = ({ cartItems, addToCart, removeFromCart }) => {
                     addToCart={addToCart}
                     removeFromCart={removeFromCart}
                 />
-            ))};
+            ))}
             <h2>Total: ${calculateTotal(cartItems).toFixed(2)}</h2>
         </Wrapper>
     );
